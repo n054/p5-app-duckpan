7382ad0df4fb8f22559e066fd29841346846c734
Robert Picard <mail@robert.io>
2012-08-02 18:04:23 -0400

    Add App::DuckPAN::Fathead with the request_fathead_output method
    This method takes a file name and a DDG::Request object and returns
    a DDG::ZeroClickInfo::Fathead object if the query matches any of the
    lines in the file.

---
+++ lib/App/DuckPAN/Fathead.pm
@@ -0,0 +1,33 @@
+package App::DuckPAN::Fathead;
+
+use Moo;
+use IO::All;
+use DDG::ZeroClickInfo::Fathead;
+
+=method request_fathead_output
+
+Takes a file name and DDG::Request object and returns a
+DDG::ZeroClickInfo::Fathead object if there is a match.
+
+=cut
+
+sub request_fathead_output {
+    my ( $request, $file_name ) = @_;
+
+    my $file = io $file_name; 
+
+    $file->chomp;
+    
+    while ( my $line = $file->getline ) {
+        my $title = ${split( /\t/, $line )}[0];
+        
+        # Return the result if the query matches the title
+        if ( $title eq $request->query_clean ) {
+            return DDG::ZeroClickInfo::Fathead->new_via_output($line);
+        }
+    }
+
+    return;
+}
+
+1;
